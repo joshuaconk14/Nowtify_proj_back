9dd0adaa7f9e1f6cb08ba696545801a14986db81
Joshua Conklin <joshuaconk14>
2024-12-10 13:34:02 -0800

    new

--- server.py
+++ server.py
@@ -5,13 +5,13 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api', methods=['GET'])
+@app.route('/api')
 def home():
     return jsonify(message="Hello from Flask backend!"), 200
 
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port)
-
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+# if __name__ == "__main__":
+#     app.run()
