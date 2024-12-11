612ac869461ccb70e9954fa7b50940423f53a6d0
Joshua Conklin <joshuaconk14>
2024-12-10 18:08:07 -0800

    Deploy

--- server.py
+++ server.py
@@ -5,13 +5,10 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api', methods=['GET'])
+@app.route('/aa', methods=['GET'])
 def home():
-    return jsonify({'message': 'homepage'}), 200
+    return jsonify({'message': 'homepage'}), 201
 
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
     app.run()
